709ddd2e4c3cff23e3f4ce411a4fc6ae49465dec
Boakai <boakaidkamara@gmail.com>
2022-06-20 22:16:11 +0000

    created db file

--- boakai_dorbor_kamara/boakai_dorbor_kamara/settings.py
+++ boakai_dorbor_kamara/boakai_dorbor_kamara/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2i%5=p3yufg*qj#jd)#8)!)%7-atd6hw6#(6=ang!ow&-^3rqc'
+SECRET_KEY = 'django-insecure--c@hu9alk4fgx30l5=b8j+oopx#bnvroj=3un9tyb3#kh7d1ka'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
